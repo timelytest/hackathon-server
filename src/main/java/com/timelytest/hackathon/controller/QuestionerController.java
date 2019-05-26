900c9fded28def72f2ef1920786f64c6a96e1497
tubinyan <1169236805@qq.com>
2019-05-26 14:51:26 +0800

    change QuestionerController, AnswerController

--- src/main/java/com/timelytest/hackathon/controller/QuestionerController.java
+++ src/main/java/com/timelytest/hackathon/controller/QuestionerController.java
@@ -1,21 +1,36 @@
 package com.timelytest.hackathon.controller;
 
 import com.timelytest.hackathon.bean.QuestionPublishBean;
+import com.timelytest.hackathon.entity.Question;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/question")
 public class QuestionerController {
 
     @PostMapping("/publish")
+    // 发布问题
     public String publish(@RequestBody QuestionPublishBean questionPublishBean){
         return null;
     }
 
+    @PostMapping("/modify")
+    // 修改问题
+    public String modify(@RequestParam int questionId, @RequestBody QuestionPublishBean questionPublishBean){
+        return null;
+    }
+
     @PostMapping("/adopt")
-    public String adopt(int questionId, int answerId){
+    // 采纳回答
+    public String adopt(@RequestParam int questionId, @RequestParam  int answerId){
+        return null;
+    }
+
+    @GetMapping("/list/publish")
+    // 查看发布过的问题
+    public List<Question> getQuestionList(@RequestParam String email){
         return null;
     }
 }
