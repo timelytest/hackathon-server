178a8c59cf35408307bcc378297dd1ad046de7d7
161250142 <1663451070@qq.com>
2019-05-26 23:02:31 +0800

    23.02

--- src/main/java/com/timelytest/hackathon/controller/AnswerController.java
+++ src/main/java/com/timelytest/hackathon/controller/AnswerController.java
@@ -2,22 +2,29 @@ package com.timelytest.hackathon.controller;
 
 import com.timelytest.hackathon.entity.Answer;
 import com.timelytest.hackathon.entity.Question;
+import com.timelytest.hackathon.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class AnswerController {
+    private final AnswerService answerService;
+    @Autowired
+    public AnswerController(AnswerService answerService){
+        this.answerService=answerService;
+    }
     @PostMapping("/question/list/type")
     // 根据 type 为用户提供推荐的问题列表
     public List<Question> getQuestionListByType(@RequestParam String type){
-        return null;
+        return answerService.getQuestionListByType(type);
     }
 
-    @PostMapping("/question/list/recommendation")
-    // 根据 email 为用户提供推荐的问题列表
+    @GetMapping("/question/list/recommendation")
+    // 根据 key 为用户提供推荐的问题列表
     public List<Question> getQuestionListByRecommendation(@RequestParam String keywords){
-        return null;
+        return answerService.getQuestionListByRecommendation(keywords);
     }
 
     @PostMapping("/question/answer")
--- src/main/java/com/timelytest/hackathon/serviceImpl/AnswerServiceImpl.java
+++ src/main/java/com/timelytest/hackathon/serviceImpl/AnswerServiceImpl.java
@@ -2,22 +2,28 @@ package com.timelytest.hackathon.serviceImpl;
 
 import com.timelytest.hackathon.entity.Answer;
 import com.timelytest.hackathon.entity.Question;
+import com.timelytest.hackathon.repository.QuestionRepository;
 import com.timelytest.hackathon.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AnswerServiceImpl implements AnswerService {
-
+    final QuestionRepository questionRepository;
+    @Autowired
+    public AnswerServiceImpl(QuestionRepository questionRepository){
+        this.questionRepository=questionRepository;
+    }
     @Override
     public List<Question> getQuestionListByType(String type) {
-        return null;
+        return questionRepository.findByTypeAndAndClosed(type,false);
     }
 
     @Override
     public List<Question> getQuestionListByRecommendation(String keywords) {
-        return null;
+        return questionRepository.findByKeyAndClosed(keywords,false);
     }
 
     @Override
